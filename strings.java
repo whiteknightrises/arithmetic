d20df0cd68c3d1c4a0fe8e691061a0b71ff580e4
whiteknightrises <161366053+whiteknightrises@users.noreply.github.com>
2024-03-10 19:23:02 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,17 @@
+package javaprograms;
+
+public class strings {
+
+	public static void main(String[] args) {
+		String x = "java";
+		String y = "Java";
+        int k = x.compareTo(y);// Returns the value of k in ASCII 
+        
+       System.out.println(x==y);// Returns TRUE OR FALSE
+       System.out.println(x.equals(y));// Returns TRUE OR FALSE
+        
+	}
+        
+	
+
+}
